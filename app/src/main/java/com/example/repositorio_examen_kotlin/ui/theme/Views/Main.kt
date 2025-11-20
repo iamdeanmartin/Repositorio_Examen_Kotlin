d36f15799c6801fe08471fce9664f32addd85d1a
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-11-20 16:06:40 +0100

    Insertamos todas las imagenes, ponemos también los checkboxes y además de eso ponemos el switch junto a su texto.

--- app/src/main/java/com/example/repositorio_examen_kotlin/ui/theme/Views/Main.kt
+++ app/src/main/java/com/example/repositorio_examen_kotlin/ui/theme/Views/Main.kt
@@ -14,7 +14,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 
+import androidx.compose.foundation.layout.width
+
 import androidx.compose.material3.Slider
+
+import com.example.repositorio_examen_kotlin.R
+
+import androidx.compose.ui.res.painterResource
+
+import androidx.compose.foundation.Image
+
+import androidx.compose.material3.Switch
+
+import androidx.compose.material3.Checkbox
 
 import androidx.compose.ui.unit.sp
 
@@ -38,11 +50,27 @@ fun main(modifier:Modifier = Modifier) {
 
     var nombre by rememberSaveable { mutableStateOf("") }
 
+    var switchState by rememberSaveable { mutableStateOf(true) }
+
     var minimo by rememberSaveable { mutableStateOf(0) }
 
     var maximo by rememberSaveable { mutableStateOf(0) }
 
     var cantidadInventario by rememberSaveable { mutableStateOf(0) }
+
+    var sandia by rememberSaveable { mutableStateOf(false) }
+
+    var platano by rememberSaveable { mutableStateOf(false) }
+
+    var champi by rememberSaveable { mutableStateOf(false) }
+
+    var zanahoria by rememberSaveable { mutableStateOf(false) }
+
+    var kiwi by rememberSaveable { mutableStateOf(false) }
+
+    var pastel by rememberSaveable { mutableStateOf(false) }
+
+    var cantidadMax by rememberSaveable { mutableStateOf(cantidadInventario) }
 
     Box(
         modifier = Modifier
@@ -143,13 +171,140 @@ fun main(modifier:Modifier = Modifier) {
                     fontSize = 12.sp,
                 )
 
-                TextField(
-                    value = nombre,
-                    onValueChange = {nombre = it},
-                    label = { Text(text = "1") }
+                Text (
+                    text = "$cantidadInventario"
                 )
             }
 
+            Column(
+                modifier = Modifier
+                    .padding(8.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Checkbox(
+                        checked = sandia,
+                        onCheckedChange = { sandia = it }
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.watermelon),
+                        contentDescription = "Imagen Sandía",
+                        modifier = Modifier
+                            .height(25.dp)
+                            .width(25.dp)
+                    )
+
+                    Checkbox(
+                        checked = platano,
+                        onCheckedChange = { platano = it }
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.banana),
+                        contentDescription = "Imagen Platano",
+                        modifier = Modifier
+                            .height(25.dp)
+                            .width(25.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Checkbox(
+                        checked = champi,
+                        onCheckedChange = { champi = it }
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.mushroom),
+                        contentDescription = "Imagen Champi",
+                        modifier = Modifier
+                            .height(25.dp)
+                            .width(25.dp)
+                    )
+
+                    Checkbox(
+                        checked = zanahoria,
+                        onCheckedChange = { zanahoria = it }
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.carrot),
+                        contentDescription = "Imagen Zanahoria",
+                        modifier = Modifier
+                            .height(25.dp)
+                            .width(25.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Checkbox(
+                        checked = kiwi,
+                        onCheckedChange = { kiwi = it }
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.kiwi),
+                        contentDescription = "Imagen Kiwi",
+                        modifier = Modifier
+                            .height(25.dp)
+                            .width(25.dp)
+                    )
+
+                    Checkbox(
+                        checked = pastel,
+                        onCheckedChange = { pastel = it }
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.cake),
+                        contentDescription = "Imagen Pastel",
+                        modifier = Modifier
+                            .height(25.dp)
+                            .width(25.dp)
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Está permés lootejar?:",
+                    fontSize = 12.sp,
+                )
+
+                Switch(checked = switchState,
+                    onCheckedChange = { switchState = !switchState }
+                    )
+            }
 
         }
     }
