4f4e28dc2fb43305297ddda29e6e5bfb7446bbe0
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-11-20 15:26:46 +0100

    Colocamos el textfiled con su valor y lo dejamos bine alineado como en el ejercicio de ejemplo.

--- app/src/main/java/com/example/repositorio_examen_kotlin/ui/theme/Views/Main.kt
+++ app/src/main/java/com/example/repositorio_examen_kotlin/ui/theme/Views/Main.kt
@@ -1,11 +1,55 @@
 package com.example.repositorio_examen_kotlin.ui.theme.Views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.ui.unit.dp
+
 import androidx.compose.runtime.Composable
 
 import androidx.compose.ui.Modifier
+
+import androidx.compose.foundation.layout.Row
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.Text
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 
 @Composable
 
 fun main(modifier:Modifier = Modifier) {
 
+    var nombre by rememberSaveable { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .padding(top=30.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            TextField(
+                value = nombre,
+                onValueChange = {nombre = it},
+                label = { Text(text = "Nom del server de la partida...") }
+            )
+        }
+
+    }
 }
