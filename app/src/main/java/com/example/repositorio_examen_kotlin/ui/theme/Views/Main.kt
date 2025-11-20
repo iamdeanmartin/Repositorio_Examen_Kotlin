b7a3182813c940d4947105394dad09036c93de56
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-11-20 15:15:12 +0100

    Creamos el main, lo dividimos en la carpeta de views y creamos el composable inicial.

---
+++ app/src/main/java/com/example/repositorio_examen_kotlin/ui/theme/Views/Main.kt
@@ -0,0 +1,11 @@
+package com.example.repositorio_examen_kotlin.ui.theme.Views
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun main(modifier:Modifier = Modifier) {
+
+}
